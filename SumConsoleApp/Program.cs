7ee5a69b445d21dc0e5a4254cf93eeda0e894059
yichunzhao <zhaoyichun@hotmail.com>
2019-09-09 04:12:41 +0200

    Add project files.

---
+++ SumConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumConsoleApp
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double CalArea()
+        {
+            return length * width;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle a = new Rectangle(4, 5);
+            double A1 = a.CalArea();
+
+            Rectangle b = new Rectangle(3, 4);
+            double A2 = b.CalArea();
+
+            double totalArea = A1 + A2;
+            Console.WriteLine("Total Area: " + totalArea);
+            Console.ReadLine();
+        }
+    }
+}
